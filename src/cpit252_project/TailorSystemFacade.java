02ca049a08594ee7c5aef4fe959559cde2f1a6db
shahad_balbaid <sbalbaid0005@stu.kau.edu.sa>
2024-11-12 18:06:24 +0300

    modify facade

--- src/cpit252_project/TailorSystemFacade.java
+++ src/cpit252_project/TailorSystemFacade.java
@@ -7,21 +7,70 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author msbbr
  */
 public class TailorSystemFacade {
+
     private final CustomerService customerService;
+    private OrderBuilder orderBuilder;
+    private OrderDirector orderDirector;
+    private Order order;
+    private Scanner input;
+
+    public TailorSystemFacade() {
+        this.customerService = new CustomerService();
+        this.orderBuilder = new Invoice();
+        this.orderDirector = new OrderDirector(orderBuilder);
+        this.input = new Scanner(System.in);
+    }
 
     public TailorSystemFacade(CustomerService customerService) {
         this.customerService = customerService;
     }
 
-    
-    public void addCustomer(){
-        customerService.addCustomer();
+    public String getGarment() {
+        System.out.println("Enter garment type (e.g., 'dress', 'blouse', 'skirt'):");
+        return input.nextLine();
+    }
+
+    public String getFabric() {
+        System.out.println("Enter fabric type (e.g., 'cotton', 'silk', 'tulle'):");
+        return input.nextLine();
     }
     
+    public double getMeters(){
+        System.out.println("Enter the number of meters required:");
+        return input.nextDouble();
+    }
+    
+    public boolean isUrgent(){
+        int isUrgentChoice = input.nextInt();
+        return (input.nextInt() == 1);
+    }
+    
+    public int getDays(){
+        System.out.println("Enter the Expected production date");
+        return input.nextInt();
+    }
+    
+    public void makeOrder(){
+        Customer customer = customerService.addCustomer();
+        
+        String garment = getGarment();
+        String fabric = getFabric();
+        double meters = getMeters();
+        boolean urgent = isUrgent();
+        int days = getDays();
+        //orderDirector.makeOrder(customer, fabricType, garmentType, deliveryDate, finalPrice);
+        Order order = orderDirector.getOrder();
+        System.out.println(order.GenerateInvoice());
+        
+    }
+
+    public void addCustomer() {
+        customerService.addCustomer();
+    }
+
 }
