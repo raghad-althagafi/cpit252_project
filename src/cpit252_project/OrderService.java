5862a43f28fa7802a1626f565194aea2c56d14db
shahad_balbaid <sbalbaid0005@stu.kau.edu.sa>
2024-11-16 10:15:23 +0300

    modify facade

--- src/cpit252_project/OrderService.java
+++ src/cpit252_project/OrderService.java
@@ -1,6 +1,7 @@
 package cpit252_project;
 
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,48 +22,52 @@ public class OrderService {
     private OrderBuilder orderBuilder;
     private OrderDirector orderDirector;
     private Order order;
+    private PricingAndDeliveryDateService priceAndDeliery;
     
     public OrderService(){
         this.customerService = new CustomerService();
         this.orderBuilder = new Invoice();
         this.orderDirector = new OrderDirector(orderBuilder);
         this.input = new Scanner(System.in);
+        this.priceAndDeliery = new PricingAndDeliveryDateService();
     }
 
-    public String getGarment() {
-        System.out.println("Enter garment type (e.g., 'dress', 'blouse', 'skirt'):");
-        return input.nextLine();
-    }
-
-    public String getFabric() {
-        System.out.println("Enter fabric type (e.g., 'cotton', 'leather', 'crepe'):");
-        String garment = input.nextLine();
-        return garment;
-    }
-    
-    public double getMeters(){
-        System.out.println("Enter the number of meters required:");
-        return input.nextDouble();
-    }
-    
-    public boolean isUrgent(){
-        int isUrgentChoice = input.nextInt();
-        return (input.nextInt() == 1);
-    }
-    
-    public int getDays(){
-        System.out.println("Enter the Expected production date");
-        return input.nextInt();
-    }
+//    public String getGarment() {
+//        System.out.println("Enter garment type (e.g., 'dress', 'blouse', 'skirt'):");
+//        return input.nextLine();
+//    }
+//
+//    public String getFabric() {
+//        System.out.println("Enter fabric type (e.g., 'cotton', 'leather', 'crepe'):");
+//        String garment = input.nextLine();
+//        return garment;
+//    }
+//    
+//    public double getMeters(){
+//        System.out.println("Enter the number of meters required:");
+//        return input.nextDouble();
+//    }
+//    
+//    public boolean isUrgent(){
+//        int isUrgentChoice = input.nextInt();
+//        return (input.nextInt() == 1);
+//    }
+//    
+//    public int getDays(){
+//        System.out.println("Enter the Expected production date");
+//        return input.nextInt();
+//    }
     
     public void makeOrder(){
+        priceAndDeliery.calculatePricingAndDeliveryDate();
         Customer customer = customerService.addCustomer();
-        String garment = getGarment();
-        String fabric = getFabric();
-        double meters = getMeters();
-        boolean urgent = isUrgent();
-        int days = getDays();
-        //orderDirector.makeOrder(customer, fabricType, garmentType, deliveryDate, finalPrice);
+        String garment = priceAndDeliery.getGarmentType();
+        String fabric = priceAndDeliery.getFabricType();
+        //double meters = ;
+//        boolean urgent = priceAndDeliery.isUrgent();
+        LocalDate deliveryDate = priceAndDeliery.getDeliveryDate();
+        double finalPrice = priceAndDeliery.getFinalPrice();
+        orderDirector.makeOrder(customer, fabric, garment, deliveryDate, finalPrice);
         Order order = orderDirector.getOrder();
         Orders.add(order);
         System.out.println(order.GenerateInvoice()); 
